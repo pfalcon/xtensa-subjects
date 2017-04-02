c9676cefeb73921e41f73c2f6a215d29b7a86879
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2017-04-02 08:19:50 +0300

    2.0.0-p20160809/proj_init: Add support for dumping BootROM functions.
    For further processing e.g. with ScratchABlock.

--- 2.0.0-p20160809/proj_init.py
+++ 2.0.0-p20160809/proj_init.py
@@ -5,6 +5,45 @@ def make_data_arr(aspace, start, end, sz):
         addr += sz
 
 
+FUNCS_BLACKLIST = {
+    # Parse issues
+    "__addsf3",
+    "__subdf3",
+    "__divsi3",
+    "__udivsi3",
+    "__floatunsisf",
+    "__floatsidf",
+    # multiple entries issues
+    "conv_str_decimal",
+    "ets_vprintf",
+    "UartDwnLdProc",
+    "rom_set_channel_freq",
+    "rom_chip_50_set_channel",
+    # too complex, trips propagator
+    "MD5Transform",
+    "SHA1Transform",
+}
+
+
+def dump_funcs(APP):
+    import sys
+    import os
+    from scratchabit import actions
+    try:
+        os.makedirs("funcs")
+    except OSError:
+        pass
+    for addr, func in APP.aspace.iter_funcs():
+        funcname = APP.aspace.get_label(addr)
+        print("%08x %s" % (addr, funcname))
+        # Dump only BootROM funcs so far
+        if 0x40000000 <= addr < 0x40010000 and funcname not in FUNCS_BLACKLIST:
+            with open("funcs/%08x-%s.lst" % (addr, funcname), "w") as fobj:
+                actions.write_func_stream(APP, func, fobj, comments=False)
+
+    sys.exit(1)
+
+
 def main(APP):
     APP.aspace.memcpy(0x3fffc000, 0x4000e388, 0x857)
     APP.aspace.memcpy(0x3fffc860, 0x4000ebe8, 0x3fffdaac - 0x3fffc860)
@@ -15,3 +54,6 @@ def main(APP):
     #make_data_arr(APP.aspace, 0x3fffd500, 0x3fffd500 + 0x100, 1)
     #make_data_arr(APP.aspace, 0x3fffd600, 0x3fffd600 + 0x40, 1)
     #make_data_arr(APP.aspace, 0x4000e388, 0x4000fe38, 1)
+
+    # Uncomment to dump funcs on startup and exit
+    #dump_funcs(APP)
